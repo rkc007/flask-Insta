2829d7d4455bdcd395c5a81b265b6aafe265173e
rkc <rahulchauhan819@gmail.com>
2017-06-25 20:15:58 +0530

    first commit

---
+++ insta/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template
+import json
+import re
+import urllib2
+import sys
+from flask import request
+from bs4 import BeautifulSoup  # $ pip install beautifulsoup4
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def main(): 
+    return render_template('index.html')
+
+@app.route('/result',methods = ['POST'])
+def result():
+    print "hello"
+    name = request.form['text']
+    print name
+    usernames = name.split(',')
+    print usernames
+    posts = [  # fake array of posts
+        { 
+            'username':  'John', 
+            'userid': 'Beautiful day in Portland!' 
+        },
+        { 
+            'username':  'Susan', 
+            'userid': 'The Avengers movie was so cool!' 
+        }
+    ]
+    for username in usernames:
+        print  username+" : "+read_json(get_page_data(username))
+        posts.append( # fake array of posts
+        {
+            'username': username, 
+            'userid': read_json(get_page_data(username)) 
+        }
+    )
+
+    return render_template('result.html',posts=posts)
+
+def read_json(html):
+	soup = BeautifulSoup(html, "html5lib")
+	script = soup.find('script', text=re.compile('window\._sharedData'))
+	json_text = re.search(r'^\s*window\._sharedData\s*=\s*({.*?})\s*;\s*$',script.string, flags=re.DOTALL | re.MULTILINE).group(1)
+	data = json.loads(json_text)
+	return data["entry_data"]["ProfilePage"][0]["user"]["id"]
+
+
+def get_page_data(username):
+	master = "https://www.instagram.com/"
+	some_url = master+username+"/"
+	content = urllib2.urlopen(some_url).read()
+	return content
+
+
+if __name__ == "__main__":
+    app.run()
+
