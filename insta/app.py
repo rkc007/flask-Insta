65a9731ff2b03209042adc3818c79bf06693cbe6
rkc <rahulchauhan819@gmail.com>
2017-07-09 01:55:26 +0530

    changes in file//showing names also

--- insta/app.py
+++ insta/app.py
@@ -9,7 +9,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def main(): 
+def main():
     return render_template('index.html')
 
 @app.route('/result',methods = ['POST'])
@@ -20,39 +20,54 @@ def result():
     usernames = name.split(',')
     print usernames
     posts = [  # fake array of posts
-        { 
-            'username':  'John', 
-            'userid': 'Beautiful day in Portland!' 
-        },
-        { 
-            'username':  'Susan', 
-            'userid': 'The Avengers movie was so cool!' 
+        {
+            'username':  '',
+            'userid': '',
+            'userfullname': ''
         }
     ]
     for username in usernames:
-        print  username+" : "+read_json(get_page_data(username))
-        posts.append( # fake array of posts
-        {
-            'username': username, 
-            'userid': read_json(get_page_data(username)) 
-        }
-    )
+        try:
+            posts.append( # fake array of posts
+            {
+                'username': username,
+                'userid': read_json(get_page_data(username)),
+                'userfullname': read_json_name(get_page_data(username))
+            }
+        )
+        except:
+                posts.append( # fake array of posts
+                {
+                    'username': username,
+                    'userid': 'No Details(Check Username)',
+                    'userfullname': "No record"
+                }
+            )
+
 
     return render_template('result.html',posts=posts)
 
 def read_json(html):
-	soup = BeautifulSoup(html, "html5lib")
-	script = soup.find('script', text=re.compile('window\._sharedData'))
-	json_text = re.search(r'^\s*window\._sharedData\s*=\s*({.*?})\s*;\s*$',script.string, flags=re.DOTALL | re.MULTILINE).group(1)
-	data = json.loads(json_text)
-	return data["entry_data"]["ProfilePage"][0]["user"]["id"]
+    soup = BeautifulSoup(html, "html5lib")
+    script = soup.find('script', text=re.compile('window\._sharedData'))
+    json_text = re.search(r'^\s*window\._sharedData\s*=\s*({.*?})\s*;\s*$',script.string, flags=re.DOTALL | re.MULTILINE).group(1)
+    data = json.loads(json_text)
+    return data["entry_data"]["ProfilePage"][0]["user"]["id"]
+
+
+def read_json_name(html):
+    soup = BeautifulSoup(html, "html5lib")
+    script = soup.find('script', text=re.compile('window\._sharedData'))
+    json_text = re.search(r'^\s*window\._sharedData\s*=\s*({.*?})\s*;\s*$',script.string, flags=re.DOTALL | re.MULTILINE).group(1)
+    data = json.loads(json_text)
+    return data["entry_data"]["ProfilePage"][0]["user"]["full_name"]
 
 
 def get_page_data(username):
-	master = "https://www.instagram.com/"
-	some_url = master+username+"/"
-	content = urllib2.urlopen(some_url).read()
-	return content
+    master = "https://www.instagram.com/"
+    some_url = master+username+"/"
+    content = urllib2.urlopen(some_url).read()
+    return content
 
 
 if __name__ == "__main__":
